9b05a3dc3d264ea62856479b18d001c81f4a68ab
Gorkem Cadirci <gorkemcdrc07@gmail.com>
2025-11-25 11:08:22 +0300

    Frontend first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+function App() {
+    const [musteriler, setMusteriler] = useState([]);
+
+    useEffect(() => {
+        axios.get("http://localhost:5000/api/musteriler")
+            .then(res => setMusteriler(res.data))
+            .catch(err => console.error("API HATASI:", err));
+    }, []);
+
+    return (
+        <div style={{ padding: 20 }}>
+            <h1>Müşteriler</h1>
+            <ul>
+                {musteriler.map((m) => (
+                    <li key={m.MusteriID}>{m.MusteriAdi}</li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default App;
