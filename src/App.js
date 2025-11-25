1ba652671107dd0268a6d1518020b20d411f4230
Gorkem Cadirci <gorkemcdrc07@gmail.com>
2025-11-25 12:01:01 +0300

    deneme

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     const [musteriler, setMusteriler] = useState([]);
 
     useEffect(() => {
-        axios.get("http://10.110.110.97:5000/api/musteriler")
+        axios.get("https://10.110.110.97:5000/api/musteriler")
             .then(res => setMusteriler(res.data))
             .catch(err => console.error("API HATASI:", err));
     }, []);
